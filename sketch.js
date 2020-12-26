d0f2367ec1c307baa959d5ad5ecb81a036a3d23d
girish-Kuma <75837586+girish-Kuma@users.noreply.github.com>
2020-12-26 10:29:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var hexa,
+
+
+
+
+function preload() {
+    hexaimg=loadImage("polygon.png")
+}
+
+
+function setup(){
+var canvas=createCanvas(1200,400)
+
+}
+
+function draw(){
+background(brown)
+
+
+
+}
